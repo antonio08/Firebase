a97847bccb807b0f68f28c28b41bad05eae9bf7e
Antonio <antonio_lozano@outlook.com>
2020-03-04 19:07:00 -0600

    Fb-003 Add logic to take the user to the Dashboard + Dashboard Activity implemented + Logic to take the user to the Dashboard implemented + String resources updated + layout for activity_dashboard implemented

--- app/src/main/java/antonio08/com/github/firebase/MainActivity.kt
+++ app/src/main/java/antonio08/com/github/firebase/MainActivity.kt
@@ -1,12 +1,42 @@
+/**
+ * Created by Antonio Lozano on 2020-03-04.
+ */
+
 package antonio08.com.github.firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get Firebase Instance
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Check whether the user is already logged in
+        val currentUser = mAuth.currentUser
+
+        // If the current user is different from null it means is already logged in and can proceed
+        // to the Dashboard
+        if (currentUser != null)
+        {
+            takeUserToDashboard()
+        }
+    }
+
+    private fun takeUserToDashboard()
+    {
+        startActivity(Intent(this, DashboardActivity::class.java))
     }
 }
