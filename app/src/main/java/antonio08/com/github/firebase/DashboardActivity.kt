7013ad134ee08be078091241e2ccd1443bf342ae
Antonio <antonio_lozano@outlook.com>
2020-03-04 18:35:46 -0600

    Fb-002 Add Login Layout + layout for login added + Dashboard activity and layout added + Manifest updated + Resources added and updated

---
+++ app/src/main/java/antonio08/com/github/firebase/DashboardActivity.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by Antonio Lozano on 2020-03-04.
+ */
+
+package antonio08.com.github.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardActivity : AppCompatActivity() {
+}
