e0daaea9ea0d2e5cc6a363171f3cd599d21ddab7
Antonio <antonio_lozano@outlook.com>
2020-03-05 20:37:10 -0600

    Fb-004 Implement google sign in + Sing In using google implemented + Cleanup

--- app/src/main/java/antonio08/com/github/firebase/MainActivity.kt
+++ app/src/main/java/antonio08/com/github/firebase/MainActivity.kt
@@ -7,11 +7,25 @@ package antonio08.com.github.firebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mAuth : FirebaseAuth
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+    private lateinit var mGoogleSignInOptions : GoogleSignInOptions
+
+    companion object {
+        private const val RC_SIGN_IN_GOOGLE = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +33,28 @@ class MainActivity : AppCompatActivity() {
 
         // Get Firebase Instance
         mAuth = FirebaseAuth.getInstance()
+
+        // Set the basic permissions to sign in the user
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Request the information specified on the google sign in options
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions)
+
+        // Set width for sign in button
+        mSignInButton.setSize(SignInButton.SIZE_STANDARD)
+
+        mSignInButton.setOnClickListener{
+            signInUser()
+        }
     }
 
     override fun onStart() {
         super.onStart()
 
         // Check whether the user is already logged in
-        val currentUser = mAuth.currentUser
+        val currentUser = GoogleSignIn.getLastSignedInAccount(this)
 
         // If the current user is different from null it means is already logged in and can proceed
         // to the Dashboard
@@ -34,6 +63,36 @@ class MainActivity : AppCompatActivity() {
             takeUserToDashboard()
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode)
+        {
+            RC_SIGN_IN_GOOGLE -> handleSigInInResult(GoogleSignIn.getSignedInAccountFromIntent(data))
+        }
+    }
+
+
+    private fun signInUser()
+    {
+        val intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN_GOOGLE)
+    }
+
+    private fun handleSigInInResult(task : Task<GoogleSignInAccount>)
+    {
+        try
+        {
+            val googleSignInAccount = task.getResult(ApiException::class.java)
+            takeUserToDashboard()
+        }
+        catch (exception : ApiException)
+        {
+            val message = exception.message
+        }
+    }
+
 
     private fun takeUserToDashboard()
     {
